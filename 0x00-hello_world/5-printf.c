9a3726f09e5da4b233f7a10a09361850582fa9df
TSilinda <sani27101@gmail.com>
2023-02-16 17:14:22 -0800

    we did it again

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Print message
+* Return: 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
