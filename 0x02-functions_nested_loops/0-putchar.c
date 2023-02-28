da08dc7c345db8b40bb69c3c156e13185315370c
TSilinda <sani27101@gmail.com>
2023-02-28 08:33:18 -0800

    white spaces

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-<<<<<<< HEAD
 /**
  * main - print _putchar
  * Return: 0
@@ -15,25 +14,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-=======
-
-/**
- * main - Print "_putchar" followed by a new line.
- * Return: 0
- */
-
-int main(void)
-{
-	char name[] = "_putchar;
-	int i;
-
-	for (i = 0; i < 9; i++)
-	{
-		_putchar(name[i]);
-	}
-
-	_putchar('\n');
-
->>>>>>> e1ba823aa0cde245ada2db5654eb44d00aaa2ccc
 	return (0);
 }
