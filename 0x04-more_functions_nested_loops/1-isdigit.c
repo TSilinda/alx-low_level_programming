6daa98e364b3d41e88e5599934c6e1fa71d7fda7
TSilinda <sani27101@gmail.com>
2023-03-11 04:30:47 -0800

    arg values fixed

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,10 @@
 /**  _isdigit checks for a digit (0 through 9).
+ * @c: character we are checking
  * Return: 0 OR 1
  */
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 	{
 		return (1);
 	}
